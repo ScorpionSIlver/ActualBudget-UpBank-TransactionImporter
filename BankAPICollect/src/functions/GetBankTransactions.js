708f5674d09f00f4b7ef089421f5d19ddaa99011
ScorpionSilver <silverscorpion05@gmail.com>
2025-01-31 17:09:50 +1000

    Add up transaction ID to avoid false deduplication by actual budget | Return to hourly update

--- BankAPICollect/src/functions/GetBankTransactions.js
+++ BankAPICollect/src/functions/GetBankTransactions.js
@@ -182,6 +182,7 @@ async function uploadTransactions(accounts) {
                 // date: new Date(transaction.attributes.settledAt || transaction.attributes.createdAt).toISOString().split('T')[0],
                 amount: Math.round(transaction.attributes.amount.value * 100),
                 payee_name: transaction.attributes.description || 'Unknown',
+                imported_id: transaction.id,
               };
 
               if (roundUpAmount !== 0) {
@@ -377,6 +378,7 @@ async function uploadWeeklyTransactions(weeklyTransactions) {
                 //date: new Date(transaction.attributes.settledAt || transaction.attributes.createdAt).toISOString().split('T')[0],
                 amount: Math.round(transaction.attributes.amount.value * 100),
                 payee_name: transaction.attributes.description || 'Unknown',
+                imported_id: transaction.id,
               };
 
               if (roundUpAmount !== 0) {
